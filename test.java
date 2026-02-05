016f5d69bc53d20b463d2a3873c7729317ca5bd6
hamza <hamzaouragini6@gmail.com>
2026-02-05 17:15:24 +0100

    feat:f1

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.err.println("Hello, World123!");
+        System.out.println("Hello, World123!123");
+
     }
     
 }
